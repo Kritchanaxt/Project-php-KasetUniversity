2872b8dfc2d5080b6b42fcce2437c386677c1912
E3Coder <gotgothot.99@gmail.com>
2025-01-23 01:43:26 +0700

    website
    Home Login Register

--- Web Stock E3/tailwind.config.js
+++ Web Stock E3/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}",
-  "./HomePage.html"],
+  "./HomePage.html","/HomePage.html"],
   theme: {
     extend: {},
   },
