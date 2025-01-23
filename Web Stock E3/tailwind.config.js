be7847ee50a14d90115fde29b833cbcaa8049971
Wafer <145214064+Kritchanaxt@users.noreply.github.com>
2025-01-23 22:52:32 +0700

    Delete Web Stock E3 directory

--- Web Stock E3/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js}",
-  "./HomePage.html","/HomePage.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
